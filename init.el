bd4d5a3cc3ac6d46bb7c12c5d597a88767cd715a
pdkz <pidekazu@gmail.com>
2016-05-24 16:03:27 +0900

    Update init.el
    Added auto-complete-clang-async

--- init.el
+++ init.el
@@ -40,4 +40,38 @@
 ;; Set auto-complete
 (require 'auto-complete)
 (require 'auto-complete-config)
+(require 'auto-complete-clang-async)
 (ac-config-default)
+
+(add-hook 'c++-mode-common-hook
+          '(lambda()
+             (setq ac-clang-complete-executable "~/.emacs.d/clang-complete")
+             (setq ac-sources '(ac-source-clang-async))
+             (ac-clang-launch-completion-process)))
+
+(defun my-ac-config ()
+  (global-set-key "\M-/" 'ac-start)
+  ;; C-n/C-p で候補を選択
+  (define-key ac-complete-mode-map "\C-n" 'ac-next)
+  (define-key ac-complete-mode-map "\C-p" 'ac-previous)
+  (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+  (add-hook 'c++-mode-common-hook 'ac-cc-mode-setup)
+  (add-hook 'auto-complete-mode-hook 'ac-common-setup)
+  (global-auto-complete-mode t))
+(my-ac-config)
+
+;;ソースツリーにある.setings.elを読み込む
+(defun recursive-load-dir-settings (currentfile)
+  (let ((lds-dir (locate-dominating-file currentfile ".settings.el")))
+    (when lds-dir
+      (progn
+        (load-file (concat lds-dir ".settings.el"))
+        (recursive-load-dir-settings
+         (file-truename(concat lds-dir "..")))))))
+
+(defun load-dir-settings()
+  (interactive)
+  (when buffer-file-name
+    (recursive-load-dir-settings buffer-file-name)))
+
+(add-hook 'find-file-hook 'load-dir-settings)
